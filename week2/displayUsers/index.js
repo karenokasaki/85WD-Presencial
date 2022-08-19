c5cc5bfa812f82852d9bf5eb0730a33d4f5af0bf
karen okasaki <oks.karen@gmail.com>
2022-08-19 16:03:03 -0300

    assessment DisplayUser

---
+++ week2/displayUsers/index.js
@@ -0,0 +1,15 @@
+const array = [
+  "Angel",
+  "Victor",
+  "Isabella",
+  "Anahi",
+  "Vero",
+  "Katya",
+  "Gancalo",
+];
+
+function displayUsers(users) {
+  // your code
+}
+
+displayUsers(array);
